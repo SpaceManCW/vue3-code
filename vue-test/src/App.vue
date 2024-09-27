fc4d8d48fa148582d482eb941053956e2ac91397
linchuanwei1 <linchuanwei1@jd.com>
2024-09-27 10:18:51 +0800

    vue cli

---
+++ vue-test/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="changeMessage">
+    {{ message }}
+  </div>
+</template>
+
+// 标记setup告诉vue在编译的时候做一些特殊处理  以便使用组合式api
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const message = ref('Hello Vue3')
+function  changeMessage() {
+  message.value = 'Hello Vue3 setup'
+}
+
+onMounted(() => {
+  console.log('mounted');
+})
+</script>
